39f05d1987ee3a1ef477f78436de2be2aa888219
sv9274@srmist.edu.in <your.email@example.com>
2026-02-16 15:45:42 +0530

    Added program 2

---
+++ printOOPS.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This class prints "OOPS" to the console.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the program
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
