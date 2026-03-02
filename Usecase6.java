aeb051d9147494468d5bf9fe41f5af0e340db41f
sv9274@srmist.edu.in <your.email@example.com>
2026-03-02 12:53:53 +0530

    Added program6

---
+++ Usecase6.java
@@ -0,0 +1,52 @@
+/**
+ * @author 
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static String[] getOPattern(){
+        return new String[]{
+            "   ***   ",
+            "**     **",
+            "**     **",
+            "**     **",
+            "   ***   ",
+        };
+    }
+    public static String[] getO2Pattern(){
+        return new String[]{
+            "   ***   ",
+            "**     **",
+            "**     **",
+            "**     **",
+            "   ***   ",
+        };
+    }
+    public static String[] getPPattern(){
+        return new String[]{
+            "******",
+            "**    *",
+            "*     * ",
+            "****** ",
+            "**      ",
+            "**      ",
+        };
+    }  
+    public static String[] getSPattern(){
+        return new String[]{
+            "   ***** ",
+            " **      ",
+            "  *****   ",
+            "       ** ",
+            "  *****   ",
+        };
+    }
+    public static void main(String[] args) {
+        String[] oPattern = getOPattern();
+        String[] o2Pattern = getO2Pattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + " " + o2Pattern[i] + " " +  pPattern[i] + " " + sPattern[i]);
+        }
+   };
+}
