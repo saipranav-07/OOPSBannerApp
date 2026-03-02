6b2548d67aab72bd0eb322c1f3e2f3481054e627
sv9274@srmist.edu.in <your.email@example.com>
2026-03-02 17:51:54 +0530

    Addedprogram8

---
+++ Usecase8.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+
+public class OOPSBannerAppUC8Aligned {
+    static class BannerUtil {
+        public static HashMap<Character, String[]> createCharacterPatternMap() {
+            HashMap<Character, String[]> charMap = new HashMap<>();
+
+            charMap.put('O', new String[] {
+                "  ****   ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                "  ****   "
+            });
+
+            charMap.put('P', new String[] {
+                " ******  ",
+                " **   ** ",
+                " ******  ",
+                " **      ",
+                " **      "
+            });
+
+            charMap.put('S', new String[] {
+                " ******  ",
+                " **      ",
+                "  ****   ",
+                "     **  ",
+                " ******  "
+            });
+
+            return charMap;
+        }
+        public static void printMessage(String message, HashMap<Character, String[]> charMap) {
+            StringBuilder[] output = new StringBuilder[5];
+
+            for (int i = 0; i < 5; i++) {
+                output[i] = new StringBuilder();
+            }
+
+            for (char ch : message.toCharArray()) {
+                String[] pattern = charMap.get(ch);
+
+                if (pattern != null) {
+                    for (int i = 0; i < 5; i++) {
+                        output[i].append(pattern[i]).append("  ");
+                    }
+                }
+            }
+
+            for (StringBuilder line : output) {
+                System.out.println(line.toString());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<Character, String[]> charMap = BannerUtil.createCharacterPatternMap();
+        String message = "OOPS";
+        BannerUtil.printMessage(message, charMap);
+    }
+}
